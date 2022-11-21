a91b5117e7262dd94ea78010501ad3de3a998106
hezkielvalentino <hezkielvalentino99@gmail.com>
2022-11-22 01:33:55 +0700

    Upload Folder

---
+++ Code/PerulanganWhile.php
@@ -0,0 +1,8 @@
+<?php 
+echo "Perulangan 1 sampai 10 <br />";
+$i = 1;
+while ($i <= 100) {
+    echo "Perulangan ke: " . $i . '<br />';
+    $i++;
+}
+?>
---
+++ Code/php_dasar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Dasar</title>
+</head>
+<body>
+    <h1>Belajar PHP Dasar</h1>
+    <?php echo "Hello World"; ?>
+    <?php
+    $nim = "312110311";
+    $nama = "Hezkiel";
+    echo '<h2 style="color:red">Menggunakan Variabel</h2>';
+    echo "NIM  : " . $nim . "<br>";
+    echo "Nama : $nama";
+    echo 'Selamat Datang ' . $_GET['nama'];
+    ?>
+
+</body>
+</html>
